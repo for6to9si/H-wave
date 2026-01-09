f85fbae09cf2875188fd647b5935439eade1ea8f
for6to9si <for6to9si@gmail.com>
2026-01-09 07:05:43 +0700

    hysteria services

---
+++ etc/ndm/netfilter.d/002-hwave.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
+#logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
+
+[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit 0
+
+# $type is `iptables` or `ip6tables`
+# $table is `nat` or `mangle`
+/opt/etc/init.d/S96hysteria2 firewall_"$type"_"$table"
